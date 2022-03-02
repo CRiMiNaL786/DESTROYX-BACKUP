e83d32d946960c81c4e24310fd5a7376f6d86438
Md Noor <75603153+Mdnoor786@users.noreply.github.com>
2022-03-02 19:50:39 +0530

    Update and rename Lion.py to Destroy.py

--- Destroy.py
+++ Destroy.py
@@ -6,9 +6,9 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://github.com/TeamLionX/LionX && cd LionX && python3 -m userbot")
+system("git clone https://github.com/Criminal786/DESTROYX && cd DESTROYX && python3 -m userbot")
 # if got error
-print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
+print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @DESTROYXsupport")
 # Script written for stop heroku suspension
 
 
